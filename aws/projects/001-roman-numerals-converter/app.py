d2dba39cfab5b71a877c5ff5df4d8aec05d706fe
E2031-Salih <blackjohnny254@gmail.com>
2020-09-25 11:26:17 +0300

    after sqlite and sql hads-on

--- aws/projects/001-roman-numerals-converter/app.py
+++ aws/projects/001-roman-numerals-converter/app.py
@@ -42,6 +42,6 @@ def get_data_from_html():
     return render_template('result.html', developer_name = 'E2031-Salih', number_decimal = z, number_roman = sonuc)
 
 if __name__ == "__main__":
-#    app.run(debug = True)
+    #app.run(debug = True)
 
     app.run(host='0.0.0.0', port=80)
