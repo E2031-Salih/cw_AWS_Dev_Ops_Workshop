74bc217424dd1b12bd1d7b259011a1b5fdc456b4
E2031-Salih <blackjohnny254@gmail.com>
2020-11-11 08:48:06 +0300

    docker project 2 phonebook

---
+++ python/coding-challenges/cc-011-drawbox/drawbox.py
@@ -0,0 +1,7 @@
+def drawbox(entry):
+    for i in range(entry):
+        if i == 0 or i == entry-1:
+            print('#'*entry)
+        else:
+            print('#' + ' ' * (entry - 2) + '#')
+drawbox(20)
